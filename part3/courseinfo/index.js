e32bce1aaea71182bc06ce412bf879ba0431fefe
Fugante <ignisacedia@hotmail.com>
2022-05-06 11:33:38 -0500

    change notes service url and delete backend's main page response

--- part3/courseinfo/index.js
+++ part3/courseinfo/index.js
@@ -42,9 +42,6 @@ const generateId = () => (
 )
 
 
-app.get("/", (request, response) => {
-    response.send("<h1>Hello, World!</h1>")
-})
 app.get("/api/notes", (request, response) => {
     response.json(notes)
 })
